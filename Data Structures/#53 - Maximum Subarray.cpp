a3173736d00a51f30aad337eb484dc6665a3e118
Sajid Fayaz <50862864+sajidfayaz@users.noreply.github.com>
2022-01-17 20:09:57 +0530

    Delete Data Structures directory

--- Data Structures/#53 - Maximum Subarray.cpp
+++
@@ -1,9 +0,0 @@
-int maxSubArray(vector<int>& nums) {
-    int ans = nums[0];
-    int maxSum = nums[0];
-    for(int i=1; i<nums.size(); i++) {
-        maxSum = max(maxSum + nums[i], nums[i]);
-        ans = max(ans, maxSum);
-    }
-    return ans;
-}
