2710c0544ec39e1ee76098ba3e5659ba87b4a7fa
Sajid Fayaz <50862864+sajidfayaz@users.noreply.github.com>
2022-01-24 00:17:09 +0530

    Add solved problem

---
+++ Data Structures/#21 - Merge Two Sorted Lists.cpp
@@ -0,0 +1,19 @@
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    if(list1 == NULL) {
+        return list2;
+    } 
+    
+    if(list2 == NULL) {
+        return list1;
+    }
+    ListNode* head;
+    if(list1->val <= list2->val) {
+        head = list1;
+        head->next = mergeTwoLists(list1->next, list2);
+    } else {
+        head = list2;
+        head->next = mergeTwoLists(list1, list2->next);
+    }
+    
+    return head;
+}
